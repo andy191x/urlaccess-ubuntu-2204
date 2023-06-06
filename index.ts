eebeec2386de9a46aea30387909e9b408efd1efa
Andrew Davis <andy@191x.com>
2023-06-06 15:15:16 -0400

    Prettier reformatting

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,8 +7,8 @@ const app = express();
 const ip = process.env.ip ?? "127.0.0.1";
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8022;
 
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript Server");
 
     let x = 0;
     x++;
